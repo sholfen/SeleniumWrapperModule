e53f35de993b409b50a63b9cbd047ccb81070293
sholfen <sholfen@gmail.com>
2015-04-03 15:56:08 +0800

    example

---
+++ web_module_main.rb
@@ -0,0 +1,23 @@
+require_relative  'WebModule'
+
+puts 'demo:'
+
+#driver = Selenium::WebDriver.for :chrome
+driver = WebModule.GetDriver :chrome
+driver.get 'https://www.google.com.tw'
+
+WebModule.SetTextByName driver, 'q', 'asp.net'
+#element = driver.find_element :name => 'q'
+#element.send_keys 'sony'
+
+WebModule.SubmitByName driver, 'btnK'
+#element = driver.find_element :name => 'btnK'
+#puts 'element text: ' + element.attribute('value')
+#element.submit
+
+sleep 2
+driver.save_screenshot 'google.png'
+driver.get 'http://tw.yahoo.com'
+sleep 2
+driver.save_screenshot 'yahoo.png'
+driver.quit
